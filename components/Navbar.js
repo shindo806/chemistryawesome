1f465e4464cb14f53662fab42e41558d6c23547b
Shindo <shindo@Shindos-iMac.local>
2020-06-03 11:02:10 +0700

    add navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,32 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { Grid, Image, Button, Input, Icon } from 'semantic-ui-react';
 
 const Navbar = () => (
-  <nav className="navbar">
-    <Link href="/">
-      <a className="navbar-brand"> Note App </a>
-    </Link>
-    <Link href="/new">
-      <a className="create"> Create note </a>
-    </Link>
-  </nav>
+  <>
+    <Grid className='navbar' textAlign='center'>
+      <Grid.Column width={3}>
+        <Link href='/'>
+          <a>
+            <Image src='/assets/images/logo.svg' className='logo' />
+          </a>
+        </Link>
+      </Grid.Column>
+      <Grid.Column width={10}>
+        <Input
+          icon={<Icon name='search' inverted circular link />}
+          placeholder='Tìm kiếm Bài tập, Đề thi, Đề ôn tập ...'
+        />
+      </Grid.Column>
+      <Grid.Column width={3}>
+        <Button.Group>
+          <Button className='sign-in'> Sign in </Button> <Button.Or />
+          <Button color='blue' className='create-account'>
+            Create account
+          </Button>
+        </Button.Group>
+      </Grid.Column>
+    </Grid>
+  </>
 );
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,10 @@
-import { Grid, Image, Input, Icon, Button } from 'semantic-ui-react';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
     <>
       {/* Header */}
-      <Grid className='navbar'>
-        <Grid.Column width={3} className='navbar-logo--area'>
-          <Image src='/assets/images/chemistryawesome.png' className='logo' />
-        </Grid.Column>
-        <Grid.Column width={10}>
-          <Input
-            icon={<Icon name='search' inverted circular link />}
-            placeholder='Search...'
-          />
-        </Grid.Column>
-        <Grid.Column width={3}>
-          <Button.Group>
-            <Button className='sign-in'>Sign in</Button>
-            <Button.Or />
-            <Button color='blue' className='create-account'>
-              Create account
-            </Button>
-          </Button.Group>
-        </Grid.Column>
-      </Grid>
+      <Navbar />
     </>
   );
 }
